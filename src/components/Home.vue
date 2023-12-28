099634d0c232ae175885a91ccc44dc0f40dbd8df
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-12-28 23:22:48 +0530

    @psidh added json cards

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="home">
+    <img class="background-image" src="@/assets/Home.svg" alt="Home Background" />
+  </div>
+</template>
+
+<style>
+
+.background-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
+}
+</style>
