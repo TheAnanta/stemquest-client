52cd27c055fff4972bf98b88533e927ba2c88b0b
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-12-29 13:37:48 +0530

    Overall design page complete
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>
    1. Functionalities need to be added

--- src/views/ExploreView.vue
+++ src/views/ExploreView.vue
@@ -4,15 +4,22 @@
     <nav class='px-8 py-12 gap-8 flex '>
       <img src="@/assets/STEMQuestLogo.svg" alt="" class='h-14' />
       <input type="text" placeholder='Search by comic/edition'
-        class='pl-8 rounded-full outline-none border border-slate-800 py-2 px-4 w-full'>
+        class='pl-8 rounded-full outline-none border border-slate-800 py-1 px-4 w-full'>
       <img src="@/assets/dusiPfp.svg" alt="" class='w-14 h-14' />
     </nav>
     <div>
-      <div>
-        <h1 class='text-3xl font-semibold'>Explore our vast range of concepts</h1>
-        <h2>Explore our vast range of concepts</h2>
+      <div class='mx-16 mb-6 flex justify-between items-end'>
+        <div>
+          <h1 class='text-3xl font-extrabold'>Explore our vast range of concepts</h1>
+          <h2>Explore our vast range of concepts</h2>
+          </div>
+
+        <div class='flex space-x-8'>
+          <img src="@/assets/icons/sort.svg" alt='sort logo'/> <span>Sort</span>
+          <img src="@/assets/icons/filter.svg" alt='sort logo'/> <span>Filter</span>
+        </div>
       </div>
-      <div class="grid grid-cols-4 ">
+      <div class="grid grid-cols-5 mx-14 my-4">
         <SubtopicCard v-for="(card, index) in this.cards" :key="index" :item="card" />
       </div>
     </div>
