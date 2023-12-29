f621b1e1c7b79ecb3bf57aa845ada19b9783aefe
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-12-29 11:29:55 +0530

    @psidh added grid and data/json

--- src/views/ExploreView.vue
+++ src/views/ExploreView.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div class='grid grid-cols-4'>
     <SubtopicCard v-for="(card, index) in this.cards" :key="index" :item="card" />
   </div>
 </template>
 
 <script lang="ts">
 import SubtopicCard from '@/components/explore/SubtopicCard.vue';
-
+import subTopics from '@/assets/data/subtopics.json';
 export default {
   name: 'Explore',
   components: {
@@ -14,48 +14,7 @@ export default {
   },
   data() {
     return {
-      cards: [
-        {
-          cover:
-            'https://stemquest.theananta.in/science/biology/cell/mitochondria/Into%20the%20Mitochondria/cover.jpg',
-          title: 'Into the Mitochondria',
-          description: 'This is a description of the video',
-          age: {
-            min: 6,
-            max: 12,
-          },
-          createdOn: '2023-12-28 10:49:00 PM',
-          edition: {
-            editionNumber: 1,
-            updatedOn: '2023-12-28 10:52:05 PM',
-          },
-          feedback: {
-            aggregateRating: 4.5,
-            rating: {
-              5: 10,
-              4: 5,
-              3: 2,
-              2: 1,
-              1: 0,
-            },
-            reviews: [
-              {
-                name: 'Sidharth Philkhana',
-                rating: 5,
-                review:
-                  "STEMQuest's Into the Mitochondria is a game-changer for learning science. As a student, I never thought I'd find mitochondria so interesting! The comic-style learning, hands-on activities, and VR experiences made complex topics feel like an adventure. STEMQuest is revolutionizing the way we learn, making it exciting and unforgettable!",
-              },
-              {
-                name: 'Lochan Mathukumilli',
-                rating: 5,
-                review:
-                  "Into the Mitochondria has been an amazing addition to STEMQuest's collection. Witnessing children delve into the intricacies of cell biology with excitement is incredibly rewarding. The comic's blend of education and entertainment is a testament to our commitment to making STEM accessible and enjoyable. Looking forward to more discoveries with STEMQuest! Kudos to the STEMQuest team for making STEM education so engaging!",
-              },
-            ],
-          },
-        },
-        // Add more card objects as needed
-      ],
+      cards: subTopics,
     };
   },
 };
