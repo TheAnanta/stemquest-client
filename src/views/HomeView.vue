f1219a3abaee12c4cb1312d91aa732ffcf4fab40
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-12-29 08:56:10 +0530

    @psidh added the anchor tag to the student button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,8 +22,8 @@ export default {
         <div
           className="flex absolute items-center bottom-16 sm:left-40 sm:bottom-24 border-dashed rounded-lg border-white border-2 px-3 py-3">
           <p id="bold" className="absolute top-0 left-0 text-white text-base -mt-3 -ml-1 pr-2 bg-[#A8E25E]">Know More:</p>
-          <button className="bg-[#78a3f3e7] hover:bg-[#78a3f3] text-white px-2 py-1 mx-2 rounded-md z-20 duration-200">as
-            student</button>
+          <a href='/student' className="bg-[#78a3f3e7] hover:bg-[#78a3f3] text-white px-2 py-1 mx-2 rounded-md z-20 duration-200">as
+            student</a>
           <button className="bg-[#F9A587e7] hover:bg-[#F9A587] text-white px-2 py-1 mx-2 rounded-md z-20 duration-200">as
             educators</button>
         </div>
