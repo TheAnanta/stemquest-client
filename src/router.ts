f52b3b93c6de56a4febbfb38930cd4f8dc47e82e
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-12-29 14:49:01 +0530

    ID component added to and route added
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/HomeView.vue';
 import Explore from '@/views/ExploreView.vue';
+import Customize from '@/views/IdView.vue';
 
 export default createRouter({
   history: createWebHistory(),
@@ -12,6 +13,10 @@ export default createRouter({
     {
       path: '/student',
       component: () => Explore,
+    },
+    {
+      path: '/student/:id',
+      component: () => Customize,
     },
   ],
 })
