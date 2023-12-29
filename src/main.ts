2274c7de88db5abff7d10096297fd4684d19bcdf
Manas Malla <manasmalla.dev@gmail.com>
2023-12-29 11:54:38 +0530

    added google material icons for larger support
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,26 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
 import router from './router'; 
-import VueFeather from 'vue-feather';
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import { aliases, md } from 'vuetify/iconsets/md'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'md',
+    aliases,
+    sets: {
+      md,
+    },
+  },
+})
 
 const app = createApp(App);
-app.component(VueFeather.name, VueFeather);
-app.use(router).mount('#app');
+app.use(router).use(vuetify).mount('#app');
