9849fa8d29b196c6523f77b52488f2f9dbeeaabe
Manas Malla <manasmalla.dev@gmail.com>
2023-12-29 12:04:47 +0530

    fixing the header in explore page
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/views/ExploreView.vue
+++ src/views/ExploreView.vue
@@ -1,21 +1,18 @@
 <template>
   <section class='w-full'>
-    <div class=''> 
-      <nav class='px-8 py-12 flex justify-evenly items-start'>
-        <img src="@/assets/STEMQuestLogo.svg" alt="" class='w-[30%] h-[30%] pr-8' />
-        <input type="text" placeholder='Search by comic/edition' class='pl-8 rounded-full outline-none border border-slate-800 py-2 px-4 w-full'>
-        <img src="@/assets/dusiPfp.svg" alt="" class='w-[30%] h-[30%] ' />
+    <div class=''>
+      <nav class='px-8 py-12 gap-8 flex justify-evenly items-start'>
+        <img src="@/assets/STEMQuestLogo.svg" alt="" class='h-14' />
+        <input type="text" placeholder='Search by comic/edition'
+          class='pl-8 rounded-full outline-none border border-slate-800 py-2 px-4 w-full'>
+        <img src="@/assets/dusiPfp.svg" alt="" class='w-14 h-14' />
       </nav>
       <div>
         <h1 class='text-3xl font-semibold'>Explore our vast range of concepts</h1>
         <h2>Explore our vast range of concepts</h2>
       </div>
       <div class="grid grid-cols-4 ">
-        <SubtopicCard
-          v-for="(card, index) in this.cards"
-          :key="index"
-          :item="card"
-        />
+        <SubtopicCard v-for="(card, index) in this.cards" :key="index" :item="card" />
       </div>
     </div>
   </section>
